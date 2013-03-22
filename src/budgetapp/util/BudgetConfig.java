32834e621d0a97da0542717269c651ba1cd6ce42
Steen <alexander_steen@hotmail.com>
2013-03-22 20:20:02 +0100

    BudgetConfig cleaned and commented

--- src/budgetapp/util/BudgetConfig.java
+++ src/budgetapp/util/BudgetConfig.java
@@ -6,20 +6,28 @@ import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import budgetapp.main.R;
-import budgetapp.models.BudgetModel;
 import android.content.Context;
 
+/**
+ * Class for keeping track of config values. Can write to internal storage
+ * @author Steen
+ *
+ */
 public class BudgetConfig {
 
 	
 	private String currentBudgetFileName = "current_budget"; // Internal filename for current budget
 	private Context context;
+	
+	// Variables for the config values
 	private double var_dailyBudget = 0;
 	private String var_currency = "kr";
 	private boolean var_printCurrencyAfter = true;
 	private double var_exchangeRate = 1;
 	
+	/**
+	 * The available fields in the config
+	 */
 	public static enum fields
 	{
 		dailyBudget,
@@ -34,6 +42,12 @@ public class BudgetConfig {
 		readConfigFile();
 	}
 	
+	/**
+	 * Gets a double value from the class
+	 * @param theField - The enum field to fetch
+	 * @return A double from the listed enum values
+	 * @throws IllegalArgumentException() - If the field entered is not a double in the enum list
+	 */
 	public double getDoubleValue(BudgetConfig.fields theField)
 	{
 		switch(theField)
@@ -45,9 +59,14 @@ public class BudgetConfig {
 			default:
 					throw new IllegalArgumentException();
 		}
-		
 	}
 	
+	/**
+	 * Gets a String value from the class
+	 * @param theField - The enum field to fetch
+	 * @return A double from the listed enum values
+	 * @throws IllegalArgumentException() - If the field entered is not a String in the enum list
+	 */
 	public String getStringValue(BudgetConfig.fields theField)
 	{
 		switch(theField)
@@ -55,11 +74,16 @@ public class BudgetConfig {
 			case currency:
 				return var_currency;
 			default:
-					throw new IllegalArgumentException();
+				throw new IllegalArgumentException();
 		}
-		
 	}
 	
+	/**
+	 * Gets a boolean value from the class
+	 * @param theField - The enum field to fetch
+	 * @return A double from the listed enum values
+	 * @throws IllegalArgumentException() - If the field entered is not a boolean in the enum list
+	 */
 	public boolean getBooleanValue(BudgetConfig.fields theField)
 	{
 		switch(theField)
@@ -67,11 +91,17 @@ public class BudgetConfig {
 			case printCurrencyAfter:
 				return var_printCurrencyAfter;
 			default:
-					throw new IllegalArgumentException();
+				throw new IllegalArgumentException();
 		}
-		
 	}
-	public boolean writeValue(BudgetConfig.fields theField, double theValue)
+	
+	/**
+	 * Writes a double to a config variable
+	 * @param theField - Enum of the variable to write to
+	 * @param theValue - The double to write
+	 * @throws IllegalArgumentException() - If the field is not one of the double variables
+	 */
+	public void writeValue(BudgetConfig.fields theField, double theValue)
 	{
 		switch(theField)
 		{
@@ -82,13 +112,17 @@ public class BudgetConfig {
 				var_exchangeRate = theValue;
 				break;
 			default:
-				return false;
+				throw new IllegalArgumentException();
 		}
-		
-		return true;
 	}
 	
-	public boolean writeValue(BudgetConfig.fields theField, String theValue)
+	/**
+	 * Writes a String to a config variable
+	 * @param theField - Enum of the variable to write to
+	 * @param theValue - The String to write
+	 * @throws IllegalArgumentException() - If the field is not one of the String variables
+	 */
+	public void writeValue(BudgetConfig.fields theField, String theValue)
 	{
 		switch(theField)
 		{
@@ -96,13 +130,17 @@ public class BudgetConfig {
 				var_currency = theValue;
 				break;
 			default:
-				return false;
+				throw new IllegalArgumentException();
 		}
-		
-		return true;
 	}
 	
-	public boolean writeValue(BudgetConfig.fields theField, boolean theValue)
+	/**
+	 * Writes a boolean to a config variable
+	 * @param theField - Enum of the variable to write to
+	 * @param theValue - The boolean to write
+	 * @throws IllegalArgumentException() - If the field is not one of the boolean variables
+	 */
+	public void writeValue(BudgetConfig.fields theField, boolean theValue)
 	{
 		switch(theField)
 		{
@@ -110,12 +148,13 @@ public class BudgetConfig {
 				var_printCurrencyAfter = theValue;
 				break;
 			default:
-				return false;
+				throw new IllegalArgumentException();
 		}
-		
-		return true;
 	}
 	
+	/**
+	 * Reads the internal config file and parses it into variables, reads until EOF
+	 */
 	public void readConfigFile()
     {
     	try
@@ -130,8 +169,8 @@ public class BudgetConfig {
         			 try
         			 {
         				 strLine = in.readUTF();
-        				 // Second run, reading dailyBudget in old config file,
-        				 // try and parse it directly
+        				 // Second run, if the user has an old version of MrCashManger
+        				 // the dailyBudget would be here, try and parse the line directly
         				 if(counter==1) 
         				 {
         					 try
@@ -149,12 +188,9 @@ public class BudgetConfig {
         			 }
         			 catch(IOException e)
         			 {
-        				 System.out.println("Done reading config");
         				 done = true;
         			 }
         		 }
-            	 
-	           	//Close the input stream
             	 in.close();
    
         }
@@ -167,39 +203,41 @@ public class BudgetConfig {
 			e.printStackTrace();
 		}
     }
+	
 	/**
-	 * Parses a string and acts accordingly
+	 * Parses a string and puts the value in the corresponding variable
 	 * @param in
 	 */
 	private void parseString(String in)
     {
-    	
-		if(in.startsWith(fields.dailyBudget.name()+"=")) // dailyBudget
+		if(in.startsWith(fields.dailyBudget.name()+"="))
     	{
     		var_dailyBudget = Double.parseDouble(in.substring(fields.dailyBudget.name().length()+1));
     	}
-		else if(in.startsWith(fields.currency.name()+"=")) // currency
+		else if(in.startsWith(fields.currency.name()+"="))
 		{
     		var_currency = in.substring(fields.currency.name().length()+1);
 		}
-		else if(in.startsWith(fields.printCurrencyAfter.name()+"=")) // printCurrencyAfter
+		else if(in.startsWith(fields.printCurrencyAfter.name()+"="))
 		{
 			if(in.substring(fields.printCurrencyAfter.name().length()+1).equalsIgnoreCase("true"))
 				var_printCurrencyAfter = true;
 			else
 				var_printCurrencyAfter = false;
 		}
-		else if(in.startsWith(fields.exchangeRate.name()+"=")) // exchangeRate
+		else if(in.startsWith(fields.exchangeRate.name()+"="))
 		{
-			System.out.println(fields.exchangeRate.name() + "=" + in);
+			System.out.println(in);
 		}
-    	
     }
 	
+	/**
+	 * Save the values to internal file
+	 */
 	public void saveToFile()
     {
     	DataOutputStream out;
-		try {
+		try{
 			out = new DataOutputStream(context.openFileOutput(currentBudgetFileName,Context.MODE_PRIVATE));
 			
 			out.writeUTF(fields.dailyBudget.name()+"="+var_dailyBudget);
