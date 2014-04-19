2460740cb32bd29e4ef28b5dc7e52c4a3256eca7
Steen <alexander_steen@hotmail.com>
2014-04-19 16:16:03 +0200

    Messed up moneyfactory

--- src/budgetapp/util/money/MoneyFactory.java
+++ src/budgetapp/util/money/MoneyFactory.java
@@ -8,7 +8,7 @@ public class MoneyFactory {
 	 * @return
 	 */
 	public static Money createMoneyFromNewDouble(double d) {
-		return new Money(d);
+		return new Money(20);
 	}
 	
 	public static Money createMoney() {
