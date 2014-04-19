48ef24d5c4b0c75291d4f539fc0dd5def8a64f60
Steen <alexander_steen@hotmail.com>
2014-04-19 19:15:23 +0200

    Fixing the build

--- src/budgetapp/util/money/MoneyFactory.java
+++ src/budgetapp/util/money/MoneyFactory.java
@@ -8,7 +8,7 @@ public class MoneyFactory {
 	 * @return
 	 */
 	public static Money createMoneyFromNewDouble(double d) {
-		return new Money(10);
+		return new Money(d);
 	}
 	
 	public static Money createMoney() {
